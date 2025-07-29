0fb59dd971efd75ea50c76ea16945e59b23043f5
bigalow1 <valentinechizorom@gmail.com>
2025-07-29 14:02:49 +0100

    again

--- controllers/userController.js
+++ controllers/userController.js
@@ -81,7 +81,7 @@ const forLogin = async (req,res)=>{
     
         res.cookie('token',token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
+            secure:true,
             sameSite: 'None',
             maxAge: 3 * 60 * 60 * 1000 // 3 hours
         });
